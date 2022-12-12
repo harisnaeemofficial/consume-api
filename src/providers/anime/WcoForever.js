fc5659b2afcd008be8481a3b3ce100d84d4ef008
Tdanks2000 <tommydanks2000@hotmail.com>
2022-12-12 08:35:42 +0000

    wcoForever source done

--- src/providers/anime/WcoForever.js
+++ src/providers/anime/WcoForever.js
@@ -1,36 +1,93 @@
 import axios from "axios";
 import { load } from "cheerio";
-import puppeteer from "puppeteer-extra";
+import CFF from "../../utils/CF/src/CF";
+const CF = CFF.default;
+const cf = new CF(false);
 
-import StealthPlugin from "puppeteer-extra-plugin-stealth";
-
-import { executablePath } from "puppeteer";
-puppeteer.use(StealthPlugin());
+import { USER_AGENT } from "../../utils";
+import FormData from "form-data";
 
 class WcoForever {
   name = "WcoForever";
   baseUrl = "https://wcoforever.net";
 
+  search = async (query, page = 1) => {
+    const searchResults = {
+      currentPage: page,
+      hasNextPage: false,
+      results: [],
+    };
+
+    try {
+      let formData = new FormData();
+      formData.append("catara", query);
+      formData.append("konuara", "series");
+
+      const options = {
+        method: "post",
+        headers: {
+          ...formData.getHeaders(),
+          mode: "cors",
+          redirect: "follow",
+          credentials: "same-origin",
+          accept:
+            "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+          "X-Requested-With": "XMLHttpRequest",
+        },
+        body: formData,
+      };
+
+      const data = await cf.post(`${this.baseUrl}/search`, options);
+
+      const html = await data.text();
+      const $ = load(html);
+
+      const results = $("ul.items > li");
+      console.log($("ul.items ").html());
+
+      results.each((i, el) => {
+        const title = $(el).find("h2").text();
+        const id = $(el).find("a").attr("href");
+        const image = $(el).find("img").attr("src");
+        const type = $(el).find("span").text();
+
+        searchResults.results.push({
+          id,
+          title,
+          image,
+          type,
+        });
+      });
+
+      return searchResults;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   Source = async (episodeId) => {
     if (!episodeId.startsWith("http"))
       episodeId = `${this.baseUrl}/${episodeId}`;
 
-    const sources = [];
+    const sources = {
+      results: [],
+      // Needed to play the video
+      "user-agent": USER_AGENT,
+    };
+
     let option2 = {
       headers: {
+        "user-agent": USER_AGENT,
         "x-requested-with": "XMLHttpRequest",
       },
     };
 
     try {
-      const browser = await puppeteer.launch({
-        headless: true,
-        executablePath: executablePath(),
+      const data = await cf.get(episodeId, {
+        method: "GET",
       });
-      const page = await browser.newPage();
-      await page.goto(episodeId, { waitUntil: "networkidle2" });
 
-      const htmlEpisodeId = await page.content();
+      const htmlEpisodeId = await data.text();
 
       let tempReg =
         /<script>var.+?document\.write\(decodeURIComponent\(escape.+?<\/script>/gis;
@@ -71,7 +128,7 @@ class WcoForever {
 
         const server = req4?.cdn;
         if (req4.hd != "") {
-          sources.push({
+          sources.results.push({
             url: `${server}/getvid?evid=${req4.hd}`,
             name: "HD#2",
             type: "mp4",
@@ -79,7 +136,7 @@ class WcoForever {
         }
 
         if (req4.enc != "") {
-          sources.push({
+          sources.results.push({
             url: `${server}/getvid?evid=${req4.enc}`,
             name: "SD#2",
             type: "mp4",
@@ -87,7 +144,7 @@ class WcoForever {
         }
 
         if (req4.fhd != "") {
-          sources.push({
+          sources.results.push({
             url: `${server}/getvid?evid=${req4.fhd}`,
             name: "FHD#2",
             type: "mp4",
@@ -99,9 +156,9 @@ class WcoForever {
 
       let { data: req3 } = await axios.get(main, option2);
 
-      const server = req3?.cdn;
+      const server = req3?.server;
       if (req3.enc != "") {
-        sources.unshift({
+        sources.results.unshift({
           url: `${server}/getvid?evid=${req3.enc}`,
           name: "SD",
           type: "mp4",
@@ -109,7 +166,7 @@ class WcoForever {
       }
 
       if (req3.hd != "") {
-        sources.unshift({
+        sources.results.unshift({
           url: `${server}/getvid?evid=${req3.hd}`,
           name: "HD",
           type: "mp4",
@@ -117,14 +174,13 @@ class WcoForever {
       }
 
       if (req3.fhd != "") {
-        sources.unshift({
+        sources.results.unshift({
           url: `${server}/getvid?evid=${req3.fhd}`,
           name: "FHD",
           type: "mp4",
         });
       }
 
-      await browser.close();
       return sources;
     } catch (error) {
       console.log(error);
@@ -138,9 +194,7 @@ class WcoForever {
 
 (async () => {
   const wcoForever = new WcoForever();
-  const source = await wcoForever.Source(
-    "https://www.wcoforever.net/xian-wang-de-richang-shenghuo-season-3-episode-12-english-subbed"
-  );
+  const source = await wcoForever.search("naruto");
   console.log(source);
 })();
 
